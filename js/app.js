c8075e62160339eb25e29594e7ee84368785ba57
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2023-05-08 03:28:57 +0300

    betnero-04

--- js/app.js
+++ js/app.js
@@ -363,6 +363,59 @@
                 }
             }
         }
+        function formFieldsInit(options = {
+            viewPass: false,
+            autoHeight: false
+        }) {
+            document.body.addEventListener("focusin", (function(e) {
+                const targetElement = e.target;
+                if (targetElement.tagName === "INPUT" || targetElement.tagName === "TEXTAREA") {
+                    if (!targetElement.hasAttribute("data-no-focus-classes")) {
+                        targetElement.classList.add("_form-focus");
+                        targetElement.parentElement.classList.add("_form-focus");
+                    }
+                    formValidate.removeError(targetElement);
+                    targetElement.hasAttribute("data-validate") ? formValidate.removeError(targetElement) : null;
+                }
+            }));
+            document.body.addEventListener("focusout", (function(e) {
+                const targetElement = e.target;
+                if (targetElement.tagName === "INPUT" || targetElement.tagName === "TEXTAREA") {
+                    if (!targetElement.hasAttribute("data-no-focus-classes")) {
+                        targetElement.classList.remove("_form-focus");
+                        targetElement.parentElement.classList.remove("_form-focus");
+                    }
+                    targetElement.hasAttribute("data-validate") ? formValidate.validateInput(targetElement) : null;
+                }
+            }));
+            if (options.viewPass) document.addEventListener("click", (function(e) {
+                let targetElement = e.target;
+                if (targetElement.closest('[class*="__viewpass"]')) {
+                    let inputType = targetElement.classList.contains("_viewpass-active") ? "password" : "text";
+                    targetElement.parentElement.querySelector("input").setAttribute("type", inputType);
+                    targetElement.classList.toggle("_viewpass-active");
+                }
+            }));
+            if (options.autoHeight) {
+                const textareas = document.querySelectorAll("textarea[data-autoheight]");
+                if (textareas.length) {
+                    textareas.forEach((textarea => {
+                        const startHeight = textarea.hasAttribute("data-autoheight-min") ? Number(textarea.dataset.autoheightMin) : Number(textarea.offsetHeight);
+                        const maxHeight = textarea.hasAttribute("data-autoheight-max") ? Number(textarea.dataset.autoheightMax) : 1 / 0;
+                        setHeight(textarea, Math.min(startHeight, maxHeight));
+                        textarea.addEventListener("input", (() => {
+                            if (textarea.scrollHeight > startHeight) {
+                                textarea.style.height = `auto`;
+                                setHeight(textarea, Math.min(Math.max(textarea.scrollHeight, startHeight), maxHeight));
+                            }
+                        }));
+                    }));
+                    function setHeight(textarea, height) {
+                        textarea.style.height = `${height}px`;
+                    }
+                }
+            }
+        }
         let formValidate = {
             getErrors(form) {
                 let error = 0;
@@ -4692,5 +4745,9 @@
         menuInit();
         spollers();
         showMore();
+        formFieldsInit({
+            viewPass: false,
+            autoHeight: false
+        });
     })();
 })();
